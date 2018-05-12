71f39fb82600688b2f44372fec2b8edf518f492a
RonaldEAM <ronaldeam29@gmail.com>
2018-05-12 11:12:54 -0400

    Remove remote network nodes

--- src/lib/networks.js
+++ src/lib/networks.js
@@ -13,30 +13,6 @@ export const Networks = [
   },
   {
     geth: {
-      type: 'remote',
-      url: 'https://web3.gastracker.io',
-    },
-    chain: {
-      id: 61,
-      name: 'mainnet',
-    },
-    title: 'Mainnet (gastracker.io)',
-    id: 'gastracker/mainnet',
-  },
-  {
-    geth: {
-      type: 'remote',
-      url: 'https://mew.epool.io/',
-    },
-    chain: {
-      id: 61,
-      name: 'mainnet',
-    },
-    title: 'Mainnet (epool.io)',
-    id: 'epool/mainnet',
-  },
-  {
-    geth: {
       type: 'local',
       url: 'http://127.0.0.1:8545',
     },
@@ -46,18 +22,6 @@ export const Networks = [
     },
     title: 'Morden Testnet',
     id: 'local/morden',
-  },
-  {
-    geth: {
-      type: 'remote',
-      url: 'https://web3.gastracker.io/morden',
-    },
-    chain: {
-      id: 62,
-      name: 'morden',
-    },
-    title: 'Morden (gastracker.io)',
-    id: 'remote/morden',
   },
 ];
 
