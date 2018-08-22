bdb50b36359932a790e265270f87070fb56aacb0
Horaciojgg <horaciojesusgg@gmail.com>
2018-08-22 15:27:11 -0400

    Changed wallet name

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -35,7 +35,7 @@ class AboutClass extends React.Component {
         <div style={{ position: 'absolute', top: '25px', right: '25px' }}>
           <LogoIcon height="150px" width="150px" />
         </div>
-        <h2 style={{ color: muiTheme.palette.primary1Color, fontWeight: '200', paddingBottom: '0px', marginBottom: '5px' }}>Emerald Wallet</h2>
+        <h2 style={{ color: muiTheme.palette.primary1Color, fontWeight: '200', paddingBottom: '0px', marginBottom: '5px' }}>Webchain Wallet</h2>
         <div style={{ marginBottom: '20px' }}>{version}</div>
         {/* <div style={{ color: muiTheme.palette.secondaryTextColor, fontWeight: '100', lineHeight: '26px', maxWidth: '580px' }}>ETCDEVTEAM: Igor Artamonov, Isaac Ardis, Constantine Kryvomaz, Yury Gagarin, Tomasz Zdybal, Shane Jonas, Richard Schumann, Darcy Reno</div> */}
         <div style={{ paddingTop: '60px', marginBottom: '60px' }}>
