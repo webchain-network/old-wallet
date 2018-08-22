251e4b3e1a98cb717dd55f4577775976584d687e
Horaciojgg <horaciojesusgg@gmail.com>
2018-08-22 15:24:52 -0400

    Updated link to submit bug to webchain-network github

--- src/components/common/ErrorDialog/errorDialog.js
+++ src/components/common/ErrorDialog/errorDialog.js
@@ -29,7 +29,7 @@ const ErrorDialog = ({ open, error, message, handleClose, handleSubmit }) => {
       onRequestClose={handleClose}
     >
       <p>
-        <strong>ERROR:</strong> An unexpected error has occured. Please restart & update emerald wallet.
+        <strong>ERROR:</strong> An unexpected error has occured. Please restart & update Webchain wallet.
       </p>
       <p>
         The error was: {message}
@@ -62,7 +62,7 @@ export default connect(
       const title = encodeURIComponent(error.message);
       const body = encodeURIComponent(`\`\`\` \n${error.stack}\n \`\`\``);
 
-      const buttonLink = `https://github.com/ETCDEVTeam/emerald-wallet/issues/new?title=${title}&body=${body}`;
+      const buttonLink = `https://github.com/webchain-network/webchain-wallet/issues/new?title=${title}&body=${body}`;
 
       dispatch(screen.actions.openLink(buttonLink));
     },
